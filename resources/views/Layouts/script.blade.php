03e06ac89ed63ab26bf9a3e0e28f51665a0c561c
Stephane <esmel.stephane@eranoveacademy-edu.ci>
2025-01-14 12:52:06 +0000

    first commit

---
+++ resources/views/Layouts/script.blade.php
@@ -0,0 +1,24 @@
+    <!-- Vendor js -->
+    <script src="{{asset('assets/js/vendor.min.js')}}"></script>
+
+    <!-- Daterangepicker js -->
+    <script src="{{asset('assets/vendor/daterangepicker/moment.min.js')}}"></script>
+    <script src="{{asset('assets/vendor/daterangepicker/daterangepicker.js')}}"></script>
+
+    <!-- Vector Map Js -->
+    <script src="{{asset('assets/vendor/jsvectormap/js/jsvectormap.min.js')}}"></script>
+    <script src="{{asset('assets/vendor/jsvectormap/maps/world-merc.js')}}"></script>
+    <script src="{{asset('assets/vendor/jsvectormap/maps/world.js')}}"></script>
+
+    <!-- Dashboard App js -->
+    <script src="{{asset('assets/js/pages/demo.dashboard.js')}}"></script>
+
+    <!-- App js -->
+    <script src="{{asset('assets/js/app.min.js')}}"></script>
+
+
+    <!-- Apex Chart js -->
+    <script src="{{asset('assets/vendor/apexcharts/apexcharts.min.js')}}"></script>
+
+    <!-- Apex Chart RadialBar Demo js -->
+    <script src="{{asset('assets/js/pages/demo.apex-radialbar.js')}}"></script>
